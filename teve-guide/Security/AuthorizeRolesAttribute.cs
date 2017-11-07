78e1cf9dbc7860e18fe73ffdb00c944f93f3a32b
ChristerN <chni9401@student.miun.se>
2017-11-07 11:18:24 +0100

    Har lagt till log ut-funktionalitet

---
+++ teve-guide/Security/AuthorizeRolesAttribute.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using teve_guide.Data;
+using System.Web.Mvc;
+
+namespace teve_guide.Security
+{
+    public class AuthorizeRolesAttribute : AuthorizeAttribute
+    {
+        private readonly string[] userAssignedRole;
+        private DbOperations db = new DbOperations();
+        public AuthorizeRolesAttribute(params string[] roles)
+        {
+            this.userAssignedRole = roles;
+        }
+
+        protected override bool AuthorizeCore(HttpContextBase httpContext)
+        {
+            bool authorize = false;
+            foreach (var roles in userAssignedRole)
+            {
+                // authorize = db.IsUserInRole(httpContext.User.Identity.Name, roles);
+                if (authorize)
+                    return authorize;
+            }
+            return authorize;
+        }
+    }
+}
